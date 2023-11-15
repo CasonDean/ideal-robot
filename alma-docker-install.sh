038980252613dffb8cb24335df5181d4d72b7f3e
CasonDean <139411876+CasonDean@users.noreply.github.com>
2023-11-15 16:39:12 -0600

    alma-docker-install.sh

--- alma-docker-install.sh
+++ alma-docker-install.sh
@@ -1,53 +1,34 @@
 #! /bin/bash
 
 echo "Updating Packages."
-
 dnf --refresh update -y
-
 sleep 1
-
 echo "Installing Needed Tools."
-
 sleep 1
-
 dnf install yum-utils -y
-
 sleep 1
 
 echo "Adding Docker Repos"
-
 sleep 1
-
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
 sleep 1
 
 echo "Installing Docker."
-
 sleep 1
-
 dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-
 sleep 1
 
 echo "Starting Docker Daemon."
-
 sleep 1
-
 systemctl enable docker
-
 sleep 1
-
 systemctl start docker
-
 sleep 1
-
 docker version
-
 sleep 1
-
-docker run hello-world
-
+docker run hello-world --name hello-world
+sleep 1
+docker rm hello-world
 sleep 5
 
 echo "Installation of Docker complete."
